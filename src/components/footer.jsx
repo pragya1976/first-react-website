99e6e017d38bb01228ca548b3249deddb8149ea4
pragya1976 <palpragya688@gmail.com>
2023-07-04 15:42:15 +0530

    initial commit

---
+++ src/components/footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechGiant</h1>
+        <p>@all rights reserved</p>
+      </div>
+      <div>
+        <h5>Follow us</h5>
+        <a href="https://Youtube.com">Youtube</a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
